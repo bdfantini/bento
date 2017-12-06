207981d9775d6cad9963e0665fb1c9cccb86c8cf
Benjamín Fantini <bdfantini@gmail.com>
2017-12-06 09:58:04 -0500

    Generate hls manifest from sidx header

--- SDK/utils/mp4utils.py
+++ SDK/utils/mp4utils.py
@@ -15,6 +15,7 @@ import operator
 import hashlib
 import fractions
 import xml.sax.saxutils as saxutils
+import re
 
 LanguageCodeMap = {
     'aar': 'aa', 'abk': 'ab', 'afr': 'af', 'aka': 'ak', 'alb': 'sq', 'amh': 'am', 'ara': 'ar', 'arg': 'an',
@@ -530,7 +531,8 @@ class Mp4File:
                     if c1['name'] == 'mvex':
                         for c2 in c1['children']:
                             if c2['name'] == 'trex':
-                                self.tracks[c2['track id']].default_sample_duration = c2['default sample duration']
+                                track_id = c2['track id']
+                                self.tracks[track_id].default_sample_duration = c2['default sample duration']
                     elif c1['name'] == 'trak':
                         track_id = 0
                         for c2 in c1['children']:
@@ -548,84 +550,109 @@ class Mp4File:
         segment_size = 0
         segment_duration_sec = 0.0
         for atom in self.tree:
-            segment_size += atom['size']
-            if atom['name'] == 'moof':
-                trafs = FilterChildren(atom, 'traf')
-                if len(trafs) != 1:
-                    PrintErrorAndExit('ERROR: unsupported input file, more than one "traf" box in fragment')
-                tfhd = FilterChildren(trafs[0], 'tfhd')[0]
-                track = self.tracks[tfhd['track ID']]
-                track.moofs.append(segment_index)
-                segment_duration = 0
-                default_sample_duration = tfhd.get('default sample duration', track.default_sample_duration)
-                for trun in FilterChildren(trafs[0], 'trun'):
-                    track.sample_counts.append(trun['sample count'])
-                    for (name, value) in trun.items():
-                        if name[0] in '0123456789':
-                            sample_duration = -1
-                            fields = value.split(',')
-                            for field in fields:
-                                if field.startswith('d:'):
-                                    sample_duration = int(field[2:])
-                            if sample_duration == -1:
-                                sample_duration = default_sample_duration
-                            segment_duration += sample_duration
-                track.segment_scaled_durations.append(segment_duration)
-                segment_duration_sec = float(segment_duration) / float(track.timescale)
-                track.segment_durations.append(segment_duration_sec)
-                segment_index += 1
-
-                # remove the 'trun' entries to save some memory
-                for traf in trafs:
-                    traf['children'] = [x for x in traf['children'] if x['name'] != 'trun']
-
-            elif atom['name'] == 'mdat':
-                # end of fragment on 'mdat' atom
-                if track:
-                    track.segment_sizes.append(segment_size)
-                    if segment_duration_sec > 0.0:
-                        segment_bitrate = int((8.0 * float(segment_size)) / segment_duration_sec)
-                    else:
-                        segment_bitrate = 0
-                    track.segment_bitrates.append(segment_bitrate)
-                segment_size = 0
-
-        # parse the 'mfra' index if there is one and update segment durations.
-        # this is needed to deal with input files that have an 'mfra' index that
-        # does not exactly match the sample durations (because of rounding errors),
-        # which will make the Smooth Streaming URL mapping fail since the IIS Smooth Streaming
-        # server uses the 'mfra' index to locate the segments in the source .ismv file
-        mfra = FindChild(self.tree, ['mfra'])
-        if mfra:
-            for tfra in FilterChildren(mfra, 'tfra'):
-                track_id = tfra['track_ID']
-                if track_id not in self.tracks:
-                    continue
-                track = self.tracks[track_id]
-                moof_pointers = []
-                for (name, value) in tfra.items():
-                    if name.startswith('['):
-                        attributes = value.split(',')
-                        attribute_dict = {}
-                        for attribute in attributes:
-                            (attribute_name, attribute_value) = attribute.strip().split('=')
-                            attribute_dict[attribute_name] = int(attribute_value)
-                        if attribute_dict['traf_number'] == 1 and attribute_dict['trun_number'] == 1 and attribute_dict['sample_number'] == 1:
-                            # this points to the first sample of the first trun of the first traf, use it as a start time indication
-                            moof_pointers.append(attribute_dict)
-                if len(moof_pointers) > 1:
-                    for i in range(len(moof_pointers)-1):
-                        if i+1 >= len(track.moofs):
-                            break
-
-                        moof1 = self.segments[track.moofs[i]][0]
-                        moof2 = self.segments[track.moofs[i+1]][0]
-                        if moof1.position == moof_pointers[i]['moof_offset'] and moof2.position == moof_pointers[i+1]['moof_offset']:
-                            # pointers match two consecutive moofs
-                            moof_duration = moof_pointers[i+1]['time'] - moof_pointers[i]['time']
-                            moof_duration_sec = float(moof_duration) / float(track.timescale)
-                            track.segment_durations[i] = moof_duration_sec
-                            track.segment_scaled_durations[i] = moof_duration
+            if atom['name'] == 'sidx':
+                for key, value in atom.iteritems():
+                    if key.startswith('entry'):
+                        print key, value
+                        segment_index = int(re.findall(r'entry (\d+)', key)[0])
+                        print 'Segment index:', segment_index
+    
+                        segment_size = int(re.findall(r'referenced_size=(\d+)', value)[0])
+                        print 'Segment size:', segment_size
+    
+                        segment_duration = int(re.findall(r'segment_duration=(\d+)', value)[0])
+                        print 'Segment duration:', segment_duration
+    
+                        track = self.tracks[track_id]
+                        track.moofs.append(segment_index)
+                        track.segment_scaled_durations.append(segment_duration)
+                        segment_duration_sec = float(segment_duration) / float(track.timescale)
+                        track.segment_durations.append(segment_duration_sec)
+                        track.segment_sizes.append(segment_size)
+                        if segment_duration_sec > 0.0:
+                            segment_bitrate = int((8.0 * float(segment_size)) / segment_duration_sec)
+                        else:
+                            segment_bitrate = 0
+                        track.segment_bitrates.append(segment_bitrate)
+                            
+#            segment_size += atom['size']naa
+#            if atom['name'] == 'moof':
+#                trafs = FilterChildren(atom, 'traf')
+#                if len(trafs) != 1:
+#                    PrintErrorAndExit('ERROR: unsupported input file, more than one "traf" box in fragment')
+#                tfhd = FilterChildren(trafs[0], 'tfhd')[0]
+#                track = self.tracks[tfhd['track ID']]
+#                track.moofs.append(segment_index)
+#                segment_duration = 0
+#                default_sample_duration = tfhd.get('default sample duration', track.default_sample_duration)
+#                for trun in FilterChildren(trafs[0], 'trun'):
+#                    track.sample_counts.append(trun['sample count'])
+#                    for (name, value) in trun.items():
+#                        if name[0] in '0123456789':
+#                            sample_duration = -1
+#                            fields = value.split(',')
+#                            for field in fields:
+#                                if field.startswith('d:'):
+#                                    sample_duration = int(field[2:])
+#                            if sample_duration == -1:
+#                                sample_duration = default_sample_duration
+#                            segment_duration += sample_duration
+#                track.segment_scaled_durations.append(segment_duration)
+#                segment_duration_sec = float(segment_duration) / float(track.timescale)
+#                track.segment_durations.append(segment_duration_sec)
+#                segment_index += 1
+#
+#                # remove the 'trun' entries to save some memory
+#                for traf in trafs:
+#                    traf['children'] = [x for x in traf['children'] if x['name'] != 'trun']
+#
+#            elif atom['name'] == 'mdat':
+#                # end of fragment on 'mdat' atom
+#                if track:
+#                    track.segment_sizes.append(segment_size)
+#                    if segment_duration_sec > 0.0:
+#                        segment_bitrate = int((8.0 * float(segment_size)) / segment_duration_sec)
+#                    else:
+#                        segment_bitrate = 0
+#                    track.segment_bitrates.append(segment_bitrate)
+#                segment_size = 0
+#
+#        # parse the 'mfra' index if there is one and update segment durations.
+#        # this is needed to deal with input files that have an 'mfra' index that
+#        # does not exactly match the sample durations (because of rounding errors),
+#        # which will make the Smooth Streaming URL mapping fail since the IIS Smooth Streaming
+#        # server uses the 'mfra' index to locate the segments in the source .ismv file
+#        mfra = FindChild(self.tree, ['mfra'])
+#        if mfra:
+#            for tfra in FilterChildren(mfra, 'tfra'):
+#                track_id = tfra['track_ID']
+#                if track_id not in self.tracks:
+#                    continue
+#                track = self.tracks[track_id]
+#                moof_pointers = []
+#                for (name, value) in tfra.items():
+#                    if name.startswith('['):
+#                        attributes = value.split(',')
+#                        attribute_dict = {}
+#                        for attribute in attributes:
+#                            (attribute_name, attribute_value) = attribute.strip().split('=')
+#                            attribute_dict[attribute_name] = int(attribute_value)
+#                        if attribute_dict['traf_number'] == 1 and attribute_dict['trun_number'] == 1 and attribute_dict['sample_number'] == 1:
+#                            # this points to the first sample of the first trun of the first traf, use it as a start time indication
+#                            moof_pointers.append(attribute_dict)
+#                if len(moof_pointers) > 1:
+#                    for i in range(len(moof_pointers)-1):
+#                        if i+1 >= len(track.moofs):
+#                            break
+#
+#                        moof1 = self.segments[track.moofs[i]][0]
+#                        moof2 = self.segments[track.moofs[i+1]][0]
+#                        if moof1.position == moof_pointers[i]['moof_offset'] and moof2.position == moof_pointers[i+1]['moof_offset']:
+#                            # pointers match two consecutive moofs
+#                            moof_duration = moof_pointers[i+1]['time'] - moof_pointers[i]['time']
+#                            moof_duration_sec = float(moof_duration) / float(track.timescale)
+#                            track.segment_durations[i] = moof_duration_sec
+#                            track.segment_scaled_durations[i] = moof_duration
 
         # compute the total numer of samples for each track
         for track_id in self.tracks:
